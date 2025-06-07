8f4cb528d070fe8b37a7b7f68aa13c0fab247f51
kentjohnburchard <kentburchard@gmail.com>
2025-06-07 14:16:46 +1000

    Added .boltignore

--- src/pages/GridEchoMap.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import GridEchoMap from '../components/GridEchoMap';
-
-const GridEchoMapPage: React.FC = () => {
-  return <GridEchoMap intention="Align to Soul Path" />;
-};
-
-export default GridEchoMapPage;
