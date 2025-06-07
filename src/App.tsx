cb208dd7536e7c75ea74a6377ed9158cb90abe1a
kentjohnburchard <kentburchard@gmail.com>
2025-06-07 13:07:54 +1000

    Fix duplicate SacredLibraryPage import

--- src/App.tsx
+++ src/App.tsx
@@ -31,11 +31,6 @@ import UploadPage from './pages/UploadPage';
 import CircleDetailPage from './pages/CircleDetailPage';
 import ResonanceSessionPage from './pages/ResonanceSessionPage';
 import EchoCompassPage from './pages/EchoCompassPage';
-import SacredLibraryPage from './pages/SacredLibraryPage';
-import MediaPlayerPage from './pages/MediaPlayerPage';
-import PlaylistsPage from './pages/PlaylistsPage';
-import PlaylistDetailPage from './pages/PlaylistDetailPage';
-import UploadPage from './pages/UploadPage';
 import Layout from './components/layout/Layout';
 import RequireAuth from './components/auth/RequireAuth';
 import ErrorBoundary from './components/ErrorBoundary';
@@ -77,11 +72,6 @@ function App() {
                     <Route path="/sacred-library/upload" element={<RequireAuth><UploadPage /></RequireAuth>} />
                     <Route path="/sacred-circle/:circleId/session/:sessionId" element={<RequireAuth><ResonanceSessionPage /></RequireAuth>} />
                     <Route path="/echo-compass" element={<RequireAuth><EchoCompassPage /></RequireAuth>} />
-                    <Route path="/sacred-library" element={<RequireAuth><SacredLibraryPage /></RequireAuth>} />
-                    <Route path="/sacred-library/media/:id" element={<RequireAuth><MediaPlayerPage /></RequireAuth>} />
-                    <Route path="/sacred-library/playlists" element={<RequireAuth><PlaylistsPage /></RequireAuth>} />
-                    <Route path="/sacred-library/playlists/:id" element={<RequireAuth><PlaylistDetailPage /></RequireAuth>} />
-                    <Route path="/sacred-library/upload" element={<RequireAuth><UploadPage /></RequireAuth>} />
                   </Route>
                 </Routes>
               </CodexProvider>
